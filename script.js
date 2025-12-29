7dc30e012d1bcab7ffd87ef6c805c0254b5ae1a8
Тернавський Яромир Миколайович <krokocoder123@gmail.com>
2025-12-29 16:48:26 +0200

    Refactor HTML structure and update CSS selectors for game parameters and board

---
+++ script.js
@@ -0,0 +1,10 @@
+let setparamsModal = document.getElementById("params");
+let gameboardModal = document.getElementById("gameBoard");
+let startBtn = document.getElementById("start-button");
+let setParamsBtn = document.getElementById("set-params");
+let closeBoardBtn = document.getElementById("close-board");
+
+
+startBtn.onclick = function(){
+    
+}
